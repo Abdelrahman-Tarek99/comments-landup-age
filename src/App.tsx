6409670cea57c5197ddd5d5a542cbb1c59cfcd70
Abdelrahman Tarek <64597592+Abdelrahman-Tarek99@users.noreply.github.com>
2025-02-02 14:05:40 +0200

    enhance heading styles for better visibility in light and dark modes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline text-blue-800 dark:text-amber-300 dark:bg-gray-800">
+        Hello world!
+      </h1>
     </>
   );
 }
