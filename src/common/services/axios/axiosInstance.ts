7225e9d15557ee1d150e676016fffa9676f05f7d
Abdelrahman Tarek <64597592+Abdelrahman-Tarek99@users.noreply.github.com>
2025-02-11 01:43:38 +0200

    fixing throw error

--- src/common/services/axios/axiosInstance.ts
+++ src/common/services/axios/axiosInstance.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 export const BASELOCALHOST = 'http://localhost:1337';
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:1337/api/comnts', // BASEURL
+  baseURL: 'http://localhost:1337/api/comments', // BASEURL
   headers: {
     // Use your API key from your environment variables
     'Authorization': `Bearer ${import.meta.env.VITE_API_KEY}`,
